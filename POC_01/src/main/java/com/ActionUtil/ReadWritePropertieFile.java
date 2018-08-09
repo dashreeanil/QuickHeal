82bf5b74bb95fbe376c6b08851e4b2b54b81640d
Anil <dashreeanil@gmail.com>
2018-08-09 20:46:28 +0530

    09/08/2018_Excle Secripting

--- POC_01/src/main/java/com/ActionUtil/ReadWritePropertieFile.java
+++
@@ -1,5 +0,0 @@
-package com.ActionUtil;
-
-public class ReadWritePropertieFile {
-
-}
--- POC_01/src/main/java/com/Init/dataFilePath.java
+++ POC_01/src/main/java/com/Init/dataFilePath.java
@@ -1,5 +1,9 @@
 package com.Init;
 
 public interface dataFilePath {
+	String INJECTORFILEPATH="C:\\Users\\dashree\\git\\QuickHeal\\POC_01\\src\\main\\resources\\Injector\\Injector.xlsx";
+	String CACHEFILEPATH="";
+	String SERVERFILEPATH="";
+
 
 }
