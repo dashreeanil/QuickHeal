87b0b118567efbd550262654984c96efb53eac30
Anil <dashreeanil@gmail.com>
2018-08-10 12:31:33 +0530

    10/08/2018 Excle code changes

--- POC_01/src/main/java/com/Init/dataFilePath.java
+++ POC_01/src/main/java/com/Init/dataFilePath.java
@@ -1,7 +1,7 @@
 package com.Init;
 
 public interface dataFilePath {
-	String INJECTORFILEPATH="C:\\Users\\dashree\\git\\QuickHeal\\POC_01\\src\\main\\resources\\Injector\\Injector.xlsx";
+	String INJECTORFILEPATH="C:\\Users\\dashree\\git\\QuickHeal\\POC_01\\src\\main\\resources\\Cache\\Injector.xlsx";
 	String CACHEFILEPATH="";
 	String SERVERFILEPATH="";
 
---
+++ POC_01/src/main/java/com/Lib/GenericLib.java
@@ -0,0 +1,235 @@
+package com.Lib;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import java.util.logging.Logger;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class GenericLib {
+	public final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	public static String sFile;
+	public static String senvTestDataFilePath;
+//	static public String sDirPath = System.getProperty("user.dir");
+	public static String sInjectorDataFilePath = "./main/resources/Cache/ClientCache.xlsx";
+	public static String sCacheDataFilePath ="./main/resources/Cache/ClientCache.xlsx";
+	public static String sServerDataFilePath = "./main/resources/Cache/ClientCache.xlsx";
+	public static String sServerCacheDataFilePath ="./main/resources/Cache/ClientCache.xlsx";
+
+	/*
+	 * @author:Anil & Pawan
+	 * 
+	 * Description: To read the basic environment settings data from config file
+	 * based on Property file value
+	 */
+	public static String getProprtyValue(String sFile, String sKey) {
+		Properties prop = new Properties();
+		String sValue = null;
+		try {
+			InputStream input = new FileInputStream(sFile);
+			prop.load(input);
+			sValue = prop.getProperty(sKey);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sValue;
+	}
+
+	/*
+	 * @author:Anil & Pawan
+	 * 
+	 * 
+	 */
+	public static Properties getPropertyFile(String sFile) {
+		Properties prop = new Properties();
+		String sValue = null;
+		try {
+			InputStream input = new FileInputStream(sFile);
+			prop.load(input);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+
+	/*
+	 * @author:Anil & Pawan 
+	 * 
+	 * 
+	 */
+	public static void setPropertyValue(String sFile, String sKey, String sValue) {
+		Properties prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(new File(sFile));
+			prop.load(fis);
+			fis.close();
+			FileOutputStream fos = new FileOutputStream(new File(sFile));
+			prop.setProperty(sKey, sValue);
+			prop.store(fos, "Updated  file with " + "Key " + sKey + " and Value " + sValue);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * @author: Anil & Pawan 
+	 * 
+	 * Description:To read test data from excel sheet based on TestcaseID
+	 */
+	public static String[] toReadExcelData(String sFilepath, String sSheet, String urlID) {
+		String sData[] = null;
+		try {
+			FileInputStream fis = new FileInputStream(sFilepath);
+			Workbook wb = (Workbook) WorkbookFactory.create(fis);
+			Sheet sht = wb.getSheet(sSheet);
+			int iRowNum = sht.getLastRowNum();
+			for (int i = 0; i <= iRowNum; i++) {
+				if (sht.getRow(i).getCell(0).toString().equals(urlID)) {
+					int iCellNum = sht.getRow(i).getPhysicalNumberOfCells();
+					sData = new String[iCellNum];
+					for (int j = 0; j < iCellNum; j++) {
+						sData[j] = sht.getRow(i).getCell(j).getStringCellValue();
+					}
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sData;
+	}
+
+	/*
+	 * @author: Anil & Pawan 
+	 * 
+	 * Description: Method to read data based on row
+	 * header
+	 */
+
+	public static int getColumnIndex(String filepath, String sSheet, String colName) {
+		String[] firstRow = GenericLib.toReadExcelData(filepath, sSheet, "TEST_CASE_NO");
+		int index = 0;
+		for (int i = 0; i < firstRow.length; i++) {
+			if (firstRow[i].equalsIgnoreCase(colName)) {
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	/*
+	 * @author: Anil & Pawan 
+	 * 
+	 * Description: Method to read data based on row
+	 * header
+	 */
+
+	public static int getProdColumnIndex(String filepath, String sSheet, String colName) {
+		String[] firstRow = GenericLib.toReadExcelData(filepath, sSheet, "CIRCLE");
+		int index = 0;
+		for (int i = 0; i < firstRow.length; i++) {
+			if (firstRow[i].equalsIgnoreCase(colName)) {
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	
+	/*
+	 * @author: Anil & Pawan Description: Method to read data based on row
+	 * header
+	 */
+
+	public static int getHeaderColumnIndex(String filepath, String sSheet, String colName) {
+		String[] firstRow = GenericLib.toReadExcelData(filepath, sSheet, "SI No");
+		int index = 0;
+		for (int i = 0; i < firstRow.length; i++) {
+			if (firstRow[i].equalsIgnoreCase(colName)) {
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	/*
+	 * @author: Anil & Pawan 
+	 * Description:Method is used to set data in excel sheet
+	 */
+
+	public static void setCellData(String filePath, String sSheet, String sTestCaseID, String columnName, String value)
+			throws Exception {
+		int columnNumber = getColumnIndex(filePath, sSheet, columnName);
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			Workbook wb = (Workbook) WorkbookFactory.create(fis);
+			Sheet sht = wb.getSheet(sSheet);
+			// logger.info("----------Sheet " + sSheet);
+			int lastRowNum = sht.getLastRowNum();
+			for (int i = 0; i <= lastRowNum; i++) {
+				if (sht.getRow(i).getCell(0).toString().equals(sTestCaseID)) {
+					Row rowNum = sht.getRow(i);
+					Cell cell = rowNum.getCell(columnNumber);
+					if (cell == null) {
+						cell = rowNum.createCell(columnNumber);
+						cell.setCellValue(value);
+					} else {
+						cell.setCellValue(value);
+					}
+					break;
+				}
+			}
+			FileOutputStream fileOut = new FileOutputStream(filePath);
+			wb.write(fileOut);
+			fileOut.flush();
+			fileOut.close();
+		} catch (Exception e) {
+			throw (e);
+		}
+	}
+
+	/*
+	 * @author: 
+	 * Description: To split and return the array
+	 */
+	public static String[] getSplittedArray(String str, String splitChar) {
+		return str.split(splitChar);
+	}
+	/*
+	 * @author:
+	 * Description: Extract the string based on previous and next strings and occurrences
+	 */
+	public static String getString(String str, String startStr, int startOccurance, String endStr, int endOccurance) {
+		return str.substring(str.indexOf(startStr, startOccurance) + startStr.length(),
+				str.indexOf(endStr, endOccurance));
+	}
+
+	/*
+	 * @author:
+	 * Description: Extract the string based on previous and next strings
+	 */
+	public static String getString(String str, String startStr, String endStr) {
+		return str.substring(str.indexOf(startStr) + startStr.length(), str.indexOf(endStr));
+	}
+}
